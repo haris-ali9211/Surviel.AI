e006cb688ac864365b953ac3e195885188ef2512
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2022-03-09 16:08:33 +0500

    backend-geeting-cctv-stream-ver-0.2

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 Stream = require('node-rtsp-stream')
 stream = new Stream({
   name: 'name',
-  streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
+  streamUrl: 'rtsp://haris:haris123@192.168.1.108/cam/realmonitor?channel=1&subtype=1',
   wsPort: 9999,
   ffmpegOptions: { // options ffmpeg flags
     '-stats': '', // an option with no neccessary value uses a blank string
     '-r': 30 // options with required values specify the value after the key
   }
 })
-   
